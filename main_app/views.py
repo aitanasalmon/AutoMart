f23a794af1711ab4c1d9e05b45b80f8b9fe8b119
Aitana Salmon <aitanasalmon@gmail.com>
2019-09-25 18:50:26 -0500

    agregamos texto dinamico

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-   return render(request,'index.html')
+   nombre = "VW Jetta"
+   modelo = 2018
+   precio = 153000
+   color = "gris"
+  
+   dict = {"auto_nombre" : nombre,
+            "auto_modelo" : modelo,
+            "auto_precio" : precio,
+            "auto_color" : color,
+   }
+
+   return render(request,'index.html',dict)
 
 # Create your views here.
