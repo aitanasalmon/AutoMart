003ec79d4a17434b1890a695f1ed982265b6a679
Aitana Salmon <aitanasalmon@gmail.com>
2019-09-30 19:38:35 -0500

    modificacion para usar models, migraciones

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Auto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    modelo = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    img_url = models.TextField()
+
+    def __str__(self):
+        return self.nombre
 # Create your models here.
